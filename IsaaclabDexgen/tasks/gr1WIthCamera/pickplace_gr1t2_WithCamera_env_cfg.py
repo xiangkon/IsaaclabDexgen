d4e612b10430a1ad36b983712caa0761cefa3b33
xiangkon <hdhtiminf@163.com>
2025-09-21 16:32:25 +0800

    test

--- IsaaclabDexgen/tasks/gr1WIthCamera/pickplace_gr1t2_WithCamera_env_cfg.py
+++ IsaaclabDexgen/tasks/gr1WIthCamera/pickplace_gr1t2_WithCamera_env_cfg.py
@@ -131,7 +131,7 @@ class ObjectTableSceneCfg(InteractiveSceneCfg):
         spawn=sim_utils.PinholeCameraCfg(
             focal_length=7.0, focus_distance=400.0, horizontal_aperture=20.955, clipping_range=(0.1, 1.0e5)
         ),
-        offset=CameraCfg.OffsetCfg(pos=(0.12, 0.0, 0.15), rot=(0.1830127, -0.6830127, 0.6830127, -0.1830127), convention="ros"),
+        offset=CameraCfg.OffsetCfg(pos=(0.2, 0.0, 0.15), rot=(0.1830127, -0.6830127, 0.6830127, -0.1830127), convention="ros"),
     )
 # -y z w x
 ##
@@ -314,8 +314,8 @@ class ObservationsCfg:
 
         object = ObsTerm(func=mdp.object_obs)
 
-        # images_fpv = ObsTerm(func=base_mdp.image, params={"sensor_cfg": SceneEntityCfg("camera_fpv")})
-        # images_tpv = ObsTerm(func=base_mdp.image, params={"sensor_cfg": SceneEntityCfg("camera_tpv")})
+        images_fpv = ObsTerm(func=base_mdp.image, params={"sensor_cfg": SceneEntityCfg("camera_fpv")})
+        images_tpv = ObsTerm(func=base_mdp.image, params={"sensor_cfg": SceneEntityCfg("camera_tpv")})
 
         def __post_init__(self):
             self.enable_corruption = False
